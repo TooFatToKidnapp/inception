6cda5a1ec2d28a2834b5860611283d969e3b3d85
“TooFatTiKidnapp” <abdouali422@gmail.com>
2023-01-24 05:49:26 +0100

    working on wp

--- srcs/requirements/wordpress/config/wp-setup.sh
+++ srcs/requirements/wordpress/config/wp-setup.sh
@@ -5,3 +5,12 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
+
+mkdir -p /var/www/wp
+
+cd /var/www/wp
+
+wp --allow-root core download
+
+mv wp-config-sample.php wp-config.php
+
