1d5cccda6f5cbe5bc81d201b569754a2f053f265
“TooFatTiKidnapp” <abdouali422@gmail.com>
2023-01-24 14:09:42 +0100

    working on wp

--- srcs/requirements/wordpress/config/wp-setup.sh
+++ srcs/requirements/wordpress/config/wp-setup.sh
@@ -6,11 +6,29 @@ chmod +x wp-cli.phar
 
 mv wp-cli.phar /usr/local/bin/wp
 
-mkdir -p /var/www/wp
 
-cd /var/www/wp
 
-wp --allow-root core download
+#### Todo:
+# need to creat volumes for wp and mariadb
+# link them together via docker-compose file
+# mariadb must start first
+# finish wp config
+# link env variables with wp !!!
 
-mv wp-config-sample.php wp-config.php
+# check if the dir "/var/www/http/wp" dose not existes alredy
+if [ ! -d "/var/www/http/wp"];
+then
+
+	mkdir -p /var/www/http/wp
+
+	wp --allow-root core download --path=/var/www/http/wp
+
+	mv wp-config-sample.php wp-config.php
+
+	# add input from .env file
+	wp create config --dbname=USER_DATA --dbuser=ali --dbpass=Password123 --dbhost=localhost --path=/var/www/http/wp --allow-root
+
+
+
+fi
 
