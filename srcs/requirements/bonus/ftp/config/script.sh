a10fab417fb48e468df568ae53002654b173248a
“TooFatTiKidnapp” <abdouali422@gmail.com>
2023-02-04 21:14:51 +0100

    save

--- srcs/requirements/bonus/ftp/config/script.sh
+++ srcs/requirements/bonus/ftp/config/script.sh
@@ -9,25 +9,21 @@ ${FTP_USER_PASSWORD}
 ${FTP_USER_PASSWORD}
 EOF
 
-
-mkdir -p /home/${FTP_USER}/ftp
-chown nobody:nogroup /home/${FTP_USER}/ftp
-chmod a-w /home/${FTP_USER}/ftp
-mkdir -p /home/${FTP_USER}/ftp/files
-chown ${FTP_USER}:${FTP_USER} /home/${FTP_USER}/ftp/files
+chown ${FTP_USER}:${FTP_USER} /home/${FTP_USER}
 
 echo ${FTP_USER} > /etc/vsftpd.userlist
 
 sed -i '31s/.*/write_enable=YES/' /etc/vsftpd.conf
 sed -i '114s/.*/chroot_local_user=YES/' /etc/vsftpd.conf
-
+sed -i 's/listen=NO/listen=YES/g' /etc/vsftpd.conf
+sed -i 's/listen_ipv6=YES/listen_ipv6=NO/g' /etc/vsftpd.conf
 echo "local_enable=YES" >> /etc/vsftpd.conf
 echo "allow_writeable_chroot=YES" >> /etc/vsftpd.conf
 echo "pasv_enable=YES" >> /etc/vsftpd.conf
 echo "pasv_min_port=2000" >> /etc/vsftpd.conf
 echo "pasv_max_port=2005" >> /etc/vsftpd.conf
 echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
-echo "local_root=/home/aabdou/ftp" >> /etc/vsftpd.conf
+echo "local_root=/home/aabdou" >> /etc/vsftpd.conf
 
 service vsftpd stop
 sleep 5
